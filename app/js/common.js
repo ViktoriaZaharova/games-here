f346054879f9c14b1e2bab04de36f12ce30ea6d3
Viktoria <zaharova181114@gmail.com>
2021-08-23 13:08:08 +0300

    Initial commit

---
+++ app/js/common.js
@@ -0,0 +1,59 @@
+const swiper = new Swiper('.main-slider', {
+    // Optional parameters
+    // direction: 'vertical',
+    loop: true,
+    effect: "fade",
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
+
+$('ul.tabs__caption').on('click', 'li:not(.active)', function () {
+    $(this)
+        .addClass('active').siblings().removeClass('active')
+        .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+});
+
+// модальные окна (несколько)
+$(function () {
+    let overlay = $('.overlay'),
+        open_modal = $('.open_modal'),
+        close = $('.modal__close, .overlay'),
+        modal = $('.modal__div');
+
+    open_modal.on('click',function (event) {
+        event.preventDefault();
+
+        modal.css('display', 'none').animate({
+            opacity: 0,
+            top: '45%'
+        }, 200);
+
+        let div = $(this).attr('href');
+        overlay.fadeIn(400,
+            function () {
+                $(div)
+                    .css('display', 'flex')
+                    .animate({
+                        opacity: 1,
+                        top: '50%'
+                    }, 200);
+            });
+    });
+
+    close.on('click', function () {
+        modal
+            .animate({
+                    opacity: 0,
+                    top: '45%'
+                }, 200,
+                function () {
+                    $(this).css('display', 'none');
+                    overlay.fadeOut(400);
+                }
+            );
+    });
+});
+//end
